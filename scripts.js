f86b8debc651530a80deef19897aa9ba731b0a05
FredVal <49240617+FredVal@users.noreply.github.com>
2021-04-23 09:21:46 +0100

    first commit

---
+++ scripts.js
@@ -0,0 +1,26 @@
+const menuItems = document.querySelectorAll('li')
+const menuBoxes = document.querySelectorAll('.menu-item-box')
+
+// const firstMenuBox = document.getElementById('menu-item-first-box')
+// const secondMenuBox = document.getElementById('menu-item-second-box')
+// const thirdMenuBox = document.getElementById('menu-item-third-box')
+// const fourthMenuBox = document.getElementById('menu-item-fourth-box')
+
+for(let i=0; i<4; i++) {
+  menuItems[i].addEventListener('mouseover', () => {visible(menuBoxes[i])})
+  menuItems[i].addEventListener('mouseout', () => {invisible(menuBoxes[i])})
+}
+
+const visible = (el) => {
+  let a = true
+  if(a=true) {
+    el.classList.add('visible')
+  }
+}
+const invisible = (el) => {
+  let a = true
+  console.log(a)
+  if(a=true) {
+    el.classList.remove('visible')
+  }
+}
