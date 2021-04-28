8840bf0a043a942c86b90b81df3ab4816e13882c
FredVal <49240617+FredVal@users.noreply.github.com>
2021-04-28 13:16:39 +0100

    code cleaning menu boxes alignement

--- scripts.js
+++ scripts.js
@@ -1,27 +1,21 @@
 const menuItems = document.querySelectorAll('li')
 const menuBoxes = document.querySelectorAll('.menu-item-box')
 
-// const firstMenuBox = document.getElementById('menu-item-first-box')
-// const secondMenuBox = document.getElementById('menu-item-second-box')
-// const thirdMenuBox = document.getElementById('menu-item-third-box')
-// const fourthMenuBox = document.getElementById('menu-item-fourth-box')
-
-const visible = (el) => {
-  let a = true
-  if(a=true) {
-    el.classList.add('visible')
-  }
-}
-const invisible = (el) => {
-  let a = true
-  if(a=true) {
-    el.classList.remove('visible')
-  }
-}
+const visible = (el) => el.classList.add('visible')
+const invisible = (el) => el.classList.remove('visible')
 
 for(let i=0; i<4; i++) {
   menuItems[i].addEventListener('mouseover', () => {visible(menuBoxes[i])})
   menuItems[i].addEventListener('mouseout', () => {invisible(menuBoxes[i])})
   menuBoxes[i].addEventListener('mouseover', () => {visible(menuBoxes[i])})
   menuBoxes[i].addEventListener('mouseout', () => {invisible(menuBoxes[i])})
+  //set left position for menu boxes
+  menuItems[i].addEventListener('mouseover', () => {setLeftPosition(menuItems[i], menuBoxes[i])})
+}
+
+//aligns the center of the menu top item with the matching menu box axis
+const setLeftPosition = (menuItem, menuBox) => {
+  const indent1 = menuItems[0].offsetLeft
+  let menuBoxAxis = 3 //location of the menu box axis - the menu box width is divided by this variable
+  document.documentElement.style.setProperty("--menu-box-indent", (menuItem.offsetLeft-indent1-menuBox.clientWidth/menuBoxAxis+menuItem.clientWidth/2) + "px")
 }
